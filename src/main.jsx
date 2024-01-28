43d55959daab57840ee5ae5671fbba7e9244d1a9
rahathasanDev <devrahat.hasan@gmail.com>
2024-01-28 12:07:02 +0600

    carousel added and navbar modified

--- src/main.jsx
+++ src/main.jsx
@@ -12,6 +12,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
  <div className='max-w-7xl mx-auto'>
    <React.StrictMode>
    <RouterProvider router={router} />
-  </React.StrictMode>,
+  </React.StrictMode>
  </div>
 )
