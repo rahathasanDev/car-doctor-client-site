41605080a69ee9c5bbe05890a7e4841ff8ef75bc
rahathasanDev <devrahat.hasan@gmail.com>
2024-01-30 02:46:59 +0600

    services and servicesCard section done

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Banner from './Banner';
 import About from './About';
-import Services from './Services';
+import Services from './Services/Services';
 
 const Home = () => {
   return (
