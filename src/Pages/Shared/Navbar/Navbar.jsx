61f402b7368f71cc36f453e23c33b834b9f5c3de
rahathasanDev <devrahat.hasan@gmail.com>
2024-02-04 19:16:48 +0600

    bookings route added

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -16,12 +16,12 @@ const Navbar = () => {
   const navItems = <>
     <li><Link to="/">Home</Link></li>
     <li><Link to="/about">About </Link></li>
-    { user?.email ?  <>
-            <li><Link to="/bookings">My Bookings</Link></li>
-            <li><button onClick={handleLogOut}>Log out</button></li>
-        </> 
-        : <li> <Link to="/login">Login</Link> </li>
-       }
+    {user?.email ? <>
+      <li><Link to="/bookings">My Bookings</Link></li>
+      <li><button onClick={handleLogOut}>Log out</button></li>
+    </>
+      : <li> <Link to="/login">Login</Link> </li>
+    }
   </>
   return (
     <div className="navbar bg-base-100 h-32">
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,8 +3,9 @@ import Main from "../Layout/Main";
 import Home from "../Pages/Home";
 import Login from "../Pages/Login/Login";
 import SignUp from "../Pages/SignUp/SignUp";
-import CheckOut from "../Pages/CheckOut/CheckOut";
 import BookService from "../Pages/BookService/BookService";
+import Bookings from "../Pages/Bookings/Bookings";
+import PrivateRoute from "./PrivateRoutes";
 
 const router = createBrowserRouter([
   {
@@ -29,14 +30,12 @@ const router = createBrowserRouter([
         loader: ({params}) => fetch(`http://localhost:5000/services/${params.id}`)
       },
       {
-        path:'checkout/:id',
-        element:<CheckOut></CheckOut>,
-        loader: ({params}) => fetch(`http://localhost:5000/services/${params.id}`)
-      },
-      {
         path: 'bookings', 
-        element: <PrivateRoute><Bookings></Bookings></PrivateRoute>
+        element: <Bookings></Bookings>
       }
+     
+
+      
     ]
   },
 ]);
